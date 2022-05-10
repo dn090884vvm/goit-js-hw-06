83d11b16c0f4baf6c46f8912a8cb6a311f8ed040
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-05-10 15:37:05 +0300

    resolvong task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorValue = document.querySelector('.color');
+
+const changeButtonColorClick = document.querySelector('.change-color');
+
+changeButtonColorClick.addEventListener('click', onButtonChangeColorClick);
+
+function onButtonChangeColorClick() {
+  colorValue.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorValue.textContent;
+}
