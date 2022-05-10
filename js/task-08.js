514788daa05e0fe7df514dbdd01fa8f571d46b1b
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-05-10 15:04:55 +0300

    resolving task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const clientemail = event.currentTarget.elements;
+  console.log(clientemail);
+
+  if (email.value === '' || password.value === '') {
+    return alert('All fields must be filled');
+  }
+  const dataResult = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(dataResult);
+  event.currentTarget.reset();
+}
